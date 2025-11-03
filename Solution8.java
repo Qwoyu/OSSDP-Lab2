cfe7186ed2c2985375d8a68a8a811d9644bba459
Qwoyu <1958632871@qq.com>
2025-11-03 21:12:09 +0800

    fix: 修复第8题所有bug

--- Solution8.java
+++ Solution8.java
@@ -42,10 +42,12 @@ class Solution8 {
         int nr = grid.length;
         int nc = grid[0].length;
 
-        if (r < 0 || c > 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+        // 越界或遇到水则返回
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
             return;
         }
-        grid[r][c] = '1';
+        // 将陆地沉没为水，避免重复遍历
+        grid[r][c] = '0';
         dfs(grid, r - 1, c);
         dfs(grid, r + 1, c);
         dfs(grid, r, c - 1);
@@ -53,7 +55,7 @@ class Solution8 {
     }
 
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length <= 1) {
+        if (grid == null || grid.length == 0) {
             return 0;
         }
 
@@ -61,7 +63,7 @@ class Solution8 {
         int nc = grid[0].length;
         int num_islands = 0;
         for (int r = 0; r < nr; ++r) {
-            for (int c = 0; r < nc; ++c) {
+            for (int c = 0; c < nc; ++c) {
                 if (grid[r][c] == '1') {
                     ++num_islands;
                     dfs(grid, r, c);
